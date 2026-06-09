95e0f5f99041858a0dfb0398f8051d2ef856a424
Robert Michailov <robert@michailov.org>
2026-06-08 22:44:12 -0700

    Started working on the basic methods of OrderBook.cpp

--- README.md
+++ README.md
@@ -12,3 +12,16 @@
     * buy order id, sell order id, price, quantity
 * OrderBook.h: properties of the order book  
     * Add order, cancel order, get best bid, get best ask  
+
+
+Order:
+    Represents a limit order.
+
+Trade:
+    Represents an execution.
+
+OrderBook:
+    Maintains active buy and sell orders.
+
+MatchingEngine:
+    Applies matching rules and generates trades.
---
+++ src/OrderBook.cpp
@@ -0,0 +1,77 @@
+#include "OrderBook.h"
+#include <iostream>
+    // Checks whether an incoming order
+    // can match with opposite side
+bool hasMatch(const Order& ord); 
+
+// Removes orders whose quantity has reached 0
+void removeFilledOrders();
+
+// Checks whether the book has no active orders
+bool OrderBook::isEmpty() const {
+    return orderLookup.size() == 0;
+}
+
+void addOrder(Order* ord);
+void cancelOrder(int id);
+void matchOrder(Order* ord);
+
+double OrderBook::getBestBid() const {
+    if (buyOrders.empty()) {
+        return -1;
+    } 
+    return std::prev(buyOrders.end())->first;
+}
+
+double OrderBook::getBestAsk() const {
+    if (sellOrders.empty()) {
+        return -1;
+    }
+    return sellOrders.begin()->first;
+}
+
+void OrderBook::printBook() const {
+    for (const auto& entry : buyOrders) {
+
+    }
+}
+void OrderBook::printBids() const {
+    for (const auto& entry : buyOrders) {
+        
+    }
+}
+
+/*
+SELLS
+
+Price: 100.25
+    ID=7 Qty=20
+    ID=9 Qty=30
+
+Price: 100.50
+    ID=11 Qty=10
+
+--------------------------------------------
+
+BUYS
+
+Price: 100.00
+    ID=3 Qty=15
+    ID=5 Qty=25
+
+Price: 99.75
+    ID=8 Qty=20*/
+void OrderBook::printAsks() const {
+    std::cout << "SELLS\n\n";
+    for (const auto& entry : sellOrders) {
+        std::cout << "Price: " << entry.first << "\n";
+        auto q = entry.second;
+        while (!q.empty()) {
+            Order* ord = q.front();
+
+            std::cout << "  ID = " << ord->getId() << " Qty = " << ord->getQuantity() << "\n";
+            q.pop();
+        }
+    }
+}
+Order* getOrderByID(int id);        
