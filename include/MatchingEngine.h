eb79f372af1612939e842ed8d19ead3612c0cab5
Robert Michailov <robert@michailov.org>
2026-07-19 11:32:19 -0700

    MatchingEngine.h

--- include/MatchingEngine.h
+++ include/MatchingEngine.h
@@ -7,8 +7,8 @@ class MatchinEngine {
     private:
         OrderBook orderBook;
     
-        public:
-            std::vector<Trade> submitOrder(Order* ord);
-            bool cancelOrder(int id);
-            const OrderBook& getOrderBook() const;
+    public:
+        std::vector<Trade> submitOrder(Order* ord);
+        bool cancelOrder(int id);
+        const OrderBook& getOrderBook() const;
 }
