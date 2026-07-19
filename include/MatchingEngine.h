7ddef04b6b8c46dc1a01c8c6f18d59bdfbc9a0c1
Robert Michailov <robert@michailov.org>
2026-07-19 11:31:48 -0700

    MatchingEngine.h

---
+++ include/MatchingEngine.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "OrderBook.h"
+#include <vector>
+
+class MatchinEngine {
+    private:
+        OrderBook orderBook;
+    
+        public:
+            std::vector<Trade> submitOrder(Order* ord);
+            bool cancelOrder(int id);
+            const OrderBook& getOrderBook() const;
+}
